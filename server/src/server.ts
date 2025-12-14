29a5064959e457cad9d78185fcf690301741fe14
dawid <dawid566123@gmail.com>
2025-12-14 22:16:18 +0100

    simple test

---
+++ server/src/server.ts
@@ -0,0 +1,23 @@
+import dotenv from 'dotenv';
+import app from './app';
+import prisma from './utils/prisma';
+
+dotenv.config();
+
+const PORT = process.env.PORT || 3000;
+
+const startServer = async () => {
+  try {
+    await prisma.$connect();
+    console.log('✅ Connected to Database');
+
+    app.listen(PORT, () => {
+      console.log(`🚀 Server running on http://localhost:${PORT}`);
+    });
+  } catch (error) {
+    console.error('❌ Failed to start server:', error);
+    process.exit(1);
+  }
+};
+
+startServer();
---
+++ server/src/utils/prisma.ts
@@ -0,0 +1,13 @@
+import { PrismaClient } from '../generated/prisma/client';
+import { PrismaPg } from '@prisma/adapter-pg';
+
+const globalForPrisma = global as unknown as { prisma: PrismaClient };
+
+const connectionString = `${process.env.DATABASE_URL}`
+const adapter = new PrismaPg({ connectionString })
+
+export const prisma = globalForPrisma.prisma || new PrismaClient({ adapter });
+
+if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
+
+export default prisma;
