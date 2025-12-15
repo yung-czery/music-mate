e8fc3930baf5fe02f9665bafa36f883f9333baa5
dawid <dawid566123@gmail.com>
2025-12-15 23:07:28 +0100

    Revert "Merge pull request #2"
    This reverts commit 981a5a2bb8330b0c0475b8f83a0ebe5dec1bd619.

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,12 +1,13 @@
 import express from 'express';
 import cors from 'cors';
-import authRoutes from './routes/auth.routes';
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-app.use('/api/auth', authRoutes);
+app.get('/', (req, res) => {
+  res.send({ message: 'Hello World!'});
+});
 
 export default app;
