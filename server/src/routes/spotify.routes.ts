0d70e9b9f4db8f0bd58786088d67a70c9850652a
dawid <dawid566123@gmail.com>
2026-01-02 23:35:55 +0100

    middleware for refreshing spotify token

--- server/src/routes/spotify.routes.ts
+++ server/src/routes/spotify.routes.ts
@@ -1,6 +1,7 @@
 import { Router } from 'express';
 import { authenticateToken } from '../middlewares/auth.middleware';
 import { loginToSpotify, search, spotifyCallback, syncTracks } from '../controllers/spotify.controller';
+import { ensureSpotifyAuth } from '../middlewares/spotify.middleware';
 
 const router = Router();
 
@@ -9,6 +10,6 @@ router.get('/search', authenticateToken, search);
 router.get('/login', authenticateToken, loginToSpotify);
 router.get('/callback', spotifyCallback);
 
-router.post('/sync', authenticateToken, syncTracks);
+router.post('/sync', authenticateToken, ensureSpotifyAuth, syncTracks);
 
 export default router;
