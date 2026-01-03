d1f376aaddf6339c91b8e3cd1e268c6cc6ec483b
yung-czery <165099503+yung-czery@users.noreply.github.com>
2026-01-03 20:51:29 +0100

    Merge pull request #22
    * cookies for spotify sync
    
    * some npm problem
    
    * cookies fix

--- server/src/controllers/auth.controller.ts
+++ server/src/controllers/auth.controller.ts
@@ -69,7 +69,7 @@ export const login = async (req: Request, res: Response): Promise<void> => {
     res.cookie('jwt', token, {
       httpOnly: true,
       secure: process.env.NODE_ENV === 'production',
-      sameSite: 'lax',
+      sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax',
       maxAge: 24 * 60 * 60 * 1000, // 1 day
     });
 
@@ -117,7 +117,7 @@ export const logout = (req: Request, res: Response): void => {
   res.clearCookie('jwt', {
     httpOnly: true,
     secure: process.env.NODE_ENV === 'production',
-    sameSite: 'lax',
+    sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax',
   });
 
   res.status(200).json({ message: 'Logged out successfully' });
