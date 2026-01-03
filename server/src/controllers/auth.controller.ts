d74a11a0755ed66b658441a4d551ac009bf227dd
dawid <dawid566123@gmail.com>
2026-01-03 20:11:46 +0100

    cookies for spotify sync

--- server/src/controllers/auth.controller.ts
+++ server/src/controllers/auth.controller.ts
@@ -69,7 +69,7 @@ export const login = async (req: Request, res: Response): Promise<void> => {
     res.cookie('jwt', token, {
       httpOnly: true,
       secure: process.env.NODE_ENV === 'production',
-      sameSite: 'lax',
+      sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax',
       maxAge: 24 * 60 * 60 * 1000, // 1 day
     });
 
